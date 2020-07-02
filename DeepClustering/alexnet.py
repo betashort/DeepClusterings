c2da42e0bbfe173dda580871a05084a92b567a17
betashort <31877309+betashort@users.noreply.github.com>
2020-07-03 02:58:53 +0900

    a

--- DeepClustering/alexnet.py
+++ DeepClustering/alexnet.py
@@ -1,44 +1,76 @@
+import math
+
+import numpy as np
 import torch
 import torch.nn as nn
 
-#入力(224 x 224)
+__all__ = [ 'AlexNet', 'alexnet']
+ 
+# (number of filters, kernel size, stride, pad)
+CFG = {
+    '2012': [(96, 11, 4, 2), 'M', (256, 5, 1, 2), 'M', (384, 3, 1, 1), (384, 3, 1, 1), (256, 3, 1, 1), 'M']
+}
+
+
 class AlexNet(nn.Module):
-    
-    def __init__(self, in_dim, num_classes):
+    def __init__(self, features, num_classes):
         super(AlexNet, self).__init__()
-        
-        self.features = nn.Sequential(
-            nn.Conv2d(in_dim, 64, kernel_size=11, stride=4, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(64, 192, kernel_size=5, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-        )
-        
-        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
-        
-        self.classifier = nn.Sequential(
-            nn.Dropout(),
-            nn.Linear(256 * 6 * 6, 4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
-            nn.Linear(4096, num_classes),
-        )
-        
+        self.features = features
+        self.classifier = nn.Sequential(nn.Dropout(0.5),
+                            nn.Linear(256 * 6 * 6, 4096),
+                            nn.ReLU(inplace=True),
+                            nn.Dropout(0.5),
+                            nn.Linear(4096, 4096),
+                            nn.ReLU(inplace=True))
+
+        self.top_layer = nn.Linear(4096, num_classes)
+        self._initialize_weights()
+
     def forward(self, x):
+
         x = self.features(x)
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
+        x = x.view(x.size(0), 256 * 6 * 6)
         x = self.classifier(x)
-        
+        if self.top_layer:
+            x = self.top_layer(x)
         return x
+
+    def _initialize_weights(self):
+        for y, m in enumerate(self.modules()):
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                for i in range(m.out_channels):
+                    m.weight.data[i].normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.weight.data.normal_(0, 0.01)
+                m.bias.data.zero_()
+
+
+def make_layers_features(cfg, input_dim, bn):
+    layers = []
+    in_channels = input_dim
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=3, stride=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v[0], kernel_size=v[1], stride=v[2], padding=v[3])
+            if bn:
+                layers += [conv2d, nn.BatchNorm2d(v[0]), nn.ReLU(inplace=True)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v[0]
+    return nn.Sequential(*layers)
+
+
+def alexnet(in_dim, n_class,bn=True):
+    
+    features = make_layers_features(CFG['2012'], in_dim, bn)
+    
+    model = AlexNet(features, n_class)
+    
+    return model
